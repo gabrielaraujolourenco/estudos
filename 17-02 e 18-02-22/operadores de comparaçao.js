d068c8ed7a0324962b82d49290d792afe69f1688
gabriel <gabrielaraujo892@gmail.com>
2022-02-18 18:30:15 -0300

    estudos

---
+++ 17-02 e 18-02-22/operadores de comparaçao.js
@@ -0,0 +1,17 @@
+/*
+Operadores de coparação
+>Maior que 
+>=Maior ou igual que
+<Menor que 
+<=Menor ou igual que
+== igualdade *&************    
+=== igualdade estrita (Valor e tipo)
+!= diferente ***************
+!== diferente estrito (Valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = '10';
+const comp = num1 === num2;
+console.log(comp);
+
---
+++ 17-02 e 18-02-22/operadores de logica.js
@@ -0,0 +1,11 @@
+/*
+Operadores lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -. OR -> Ou
+! -> not -> Não
+*/
+// const expressaoAnd = true && true && false && true;
+// const expressaoOR = true || false;
+
+
+console.log(!false);
