7f8db16c8d41658b3d5562ad30ea44f8ce335c93
gabriel <gabrielaraujo892@gmail.com>
2022-02-19 19:29:51 -0300

    estudos

--- 17-02 e 18-02-22/if e else if.js
+++ 17-02 e 18-02-22/if e else if.js
@@ -4,4 +4,8 @@ if (numero >= 0 && numero <= 5){
     console.log('O número está entre 0 e 5.');
 } else if (numero >= 6 && numero <= 8) {
     console.log('O número está entre 0 e 5.');
-} else if (numero >= 9 && nume  )
+} else if (numero >= 9 && numero <= 11){ //verdadeiro
+    console.log('O número está entra 9 e 11.');
+}else{
+    console.log('O número não está entre 0 e 11.');
+}
