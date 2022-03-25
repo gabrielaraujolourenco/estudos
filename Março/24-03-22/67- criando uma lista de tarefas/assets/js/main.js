a68c3dbe07e92aa82942946977c0f05ffa7cd760
gabriel <gabrielaraujo892@gmail.com>
2022-03-25 00:09:23 -0300

    Estudos

---
+++ Março/24-03-22/67- criando uma lista de tarefas/assets/js/main.js
@@ -0,0 +1,88 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const bntTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi(){
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', function(e){
+    if (e.keyCode === 13 ){
+        if (!inputTarefa.value) return;
+        criarTarefa(inputTarefa.value);
+            
+    }
+})
+
+function limpaInput(){
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    //botaoApagar.classList.add('apagar');
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+
+    li.appendChild(botaoApagar);
+}
+
+// jogar na lista
+function criarTarefa(textoInput) {
+    const li = criaLi();
+li.innerText = textoInput;
+tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+        salvarTarefas();
+}
+
+
+// button
+bntTarefa.addEventListener('click',function(e){
+if (!inputTarefa.value) return;
+criarTarefa(inputTarefa.value);
+
+})
+
+
+document.addEventListener('click',function(e){
+    const el = e.target;
+    
+    if (el.classList.contains('apagar')) {
+    el.parentElement.remove();
+    salvarTarefas();
+
+    }
+})
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+    console.log(listaDeTarefas);
+
+    for (let tarefa of listaDeTarefas){
+        criarTarefa(tarefa);
+        
+    }
+}
+adicionaTarefasSalvas();
---
+++ Março/24-03-22/69-Parâmetros da função.js
@@ -0,0 +1,16 @@
+// argumentos que sustenta todos os argumentos enviados
+/*
+function funcao() {
+    let total = 0;
+    for (let argumento of arguments){
+        total += argumento;
+    }
+    console.log(total);
+
+}
+funcao(1, 2, 3, 4, 5, 6, 7,);
+*/
+function funcao (a, b, c, d, e, f) {
+    console.log(a, b, c, d, e, f);
+}
+funcao (1, 2, 3,);
